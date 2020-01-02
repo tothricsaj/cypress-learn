fb0db8e31a2f17e06643cfaa8ddb40a7b433c722
tothricsaj <tothricsaj@gmail.com>
2020-01-02 12:26:06 +0100

    get.....

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -4,5 +4,9 @@ describe('My First Test', function() {
 
       cy.contains('type').click()
       cy.url().should('include', '/commands/actions');
+
+      cy.get('.action-email')
+        .type('fake@email.com')
+        .should('have.value', 'fake@email.com')
   })
 })
