07772fa5767b8747bbb37ca34fef081d12d6bd9d
tothricsaj <tothricsaj@gmail.com>
2020-01-02 15:38:26 +0100

    pause :cow:

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,6 +2,8 @@ describe('My First Test', function() {
   it('Visit the Kitchen Sink', function() {
       cy.visit('https://example.cypress.io')
 
+      cy.pause()
+
       cy.contains('type').click()
       cy.url().should('include', '/commands/actions');
 
