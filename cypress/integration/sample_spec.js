28b28b532aa623c6f070dd9219877d14dfce9f0c
tothricsaj <tothricsaj@gmail.com>
2020-01-02 11:29:43 +0100

    cy contains - great

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,8 @@
 describe('My First Test', function() {
   it('Visit the Kitchen Sink', function() {
       cy.visit('https://example.cypress.io')
+
+      cy.contains('type')
+      cy.contains('trigger')
   })
 })
