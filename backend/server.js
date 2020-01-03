6cc133092c56a7400e877b9367c9ca0cf8d28e5c
tothricsaj <tothricsaj@gmail.com>
2020-01-03 13:58:26 +0100

    set the correct url

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,23 @@ const fs = require('fs');
 let app = http.createServer((req, res) => {
     var q = url.parse(req.url, true);
     var filename = "." + q.pathname;
-    fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+
+    console.log(`url -> ${q.pathname}`);
+    console.log(`filename -> ${filename}`);
+
+    if(q.pathname === '/') {
+        fs.readFile('./index.html', (err, data) => {
+
+            if (err) {
+              res.writeHead(404, {'Content-Type': 'text/html'});
+              return res.end("404 Not Found");
+            } 
+
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+        });
+    }
 }).listen(8080);
 
 console.log("server started port: 8080");
